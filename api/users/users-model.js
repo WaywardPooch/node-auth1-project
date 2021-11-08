7d5c9a1a67a3248c18e52e48cb68165c0b7d1794
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-08 13:17:28 -0800

    filled out users-model.js

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -12,18 +12,34 @@ const find = async () => {
 };
 
 // resolves to an ARRAY with all users that match the filter condition
-const findBy = (filter) => {
-
+const findBy = async (filter) => {
+  const filteredUsers = await db("users")
+    .where(filter);
+  return filteredUsers.map(user => {
+    return {
+      user_id: user.user_id,
+      username: user.username
+    };
+  });
 };
 
 // resolves to the user { user_id, username } with the given user_id
-const findById = (user_id) => {
-
+const findById = async (user_id) => {
+  const user = await db("users")
+    .where({ user_id })
+    .first();
+  return {
+    user_id: user.user_id,
+    username: user.username
+  };
 };
 
 // resolves to the newly inserted user { user_id, username }
-const add = (user) => {
-
+const add = async (user) => {
+  const [user_id] = await db("users")
+    .insert(user);
+  const newUser = await findById(user_id);
+  return newUser;
 };
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
