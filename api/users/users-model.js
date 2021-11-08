3431b84f82d254fc30f2c703fb433f77eb02d278
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-08 12:17:29 -0800

    hooked up GET /api/users; omitted Pwd resp

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -3,7 +3,12 @@ const db = require("./../../data/db-config");
 // resolves to an ARRAY with all users, each user having { user_id, username }
 const find = async () => {
   const users = await db("users");
-  return users;
+  return users.map(user => {
+    return {
+      user_id: user.user_id,
+      username: user.username
+    };
+  });
 };
 
 // resolves to an ARRAY with all users that match the filter condition
