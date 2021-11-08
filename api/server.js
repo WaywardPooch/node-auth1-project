2635e46126983ddc56d8c45b4605423054447d19
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-08 13:01:58 -0800

    added session store to server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,4 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const usersRouter = require("./users/users-router");
-
-/**
+/*
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
   This is achieved by setting 'saveUninitialized' to false, and by not
@@ -14,20 +9,53 @@ const usersRouter = require("./users/users-router");
 
   The session can be persisted in memory (would not be adecuate for production)
   or you can use a session store like `connect-session-knex`.
- */
+*/
 
+// Imports
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const db = require("./../data/db-config");
+const helmet = require("helmet");
+const usersRouter = require("./users/users-router");
+const Store = require("connect-session-knex")(session);
+
+// Server instantiation
 const server = express();
 
+// Middleware
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+// Session setup
+server.use(session({
+  name: "client",
+  cookie: {
+    maxAge: 1000 * 60 * 5,
+    secure: false,
+    httpOnly: false
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new Store({
+    knex: db,
+    tablename: "sessions",
+    sidfieldname: "session_id",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+}));
+
+// Routes
 server.use("/api/users", usersRouter);
 
+// Endpoints
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
+// Error handler
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
@@ -35,4 +63,5 @@ server.use((err, req, res, next) => { // eslint-disable-line
   });
 });
 
+// Exports
 module.exports = server;
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -27,6 +27,7 @@ const User = require("./users-model");
 */
 
 router.get("/",
+  restricted,
   async (req, res, next) => {
     try {
       const users = await User.find();
