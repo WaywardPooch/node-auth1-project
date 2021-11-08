3551ec86afeb1b3a67e181021a4f56e944f9c67e
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-08 11:43:10 -0800

    added find() to users model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,30 @@
-/**
-  resolves to an ARRAY with all users, each user having { user_id, username }
- */
-function find() {
+const db = require("./../../data/db-config");
 
-}
+// resolves to an ARRAY with all users, each user having { user_id, username }
+const find = async () => {
+  const users = await db("users");
+  return users;
+};
 
-/**
-  resolves to an ARRAY with all users that match the filter condition
- */
-function findBy(filter) {
+// resolves to an ARRAY with all users that match the filter condition
+const findBy = (filter) => {
 
-}
+};
 
-/**
-  resolves to the user { user_id, username } with the given user_id
- */
-function findById(user_id) {
+// resolves to the user { user_id, username } with the given user_id
+const findById = (user_id) => {
 
-}
+};
 
-/**
-  resolves to the newly inserted user { user_id, username }
- */
-function add(user) {
+// resolves to the newly inserted user { user_id, username }
+const add = (user) => {
 
-}
+};
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add
+};
